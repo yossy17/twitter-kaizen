a2057b52e036dc34c71fcaa1e289cb6c91c0d32f
Yos_sy <hiitoy0117@gmail.com>
2024-12-27 10:50:17 +0900

    細かな修正を実施 - Quotesのアイコンサイズを修正 - メインツイートID取得時 `/photo/1` が追加されている問題を修正 >> 原因はわからないがreplaceで対応

--- twitter-kaizen.user.js
+++ twitter-kaizen.user.js
@@ -13,7 +13,7 @@
 // @description:ko      트위터를 편안하게
 // @description:ru      Комфортное использование Твиттера
 // @description:de      Twitter bequem nutzen
-// @version             2.6.3.1
+// @version             2.6.4
 // @author              Yos_sy
 // @match               https://x.com/*
 // @namespace           http://tampermonkey.net/
@@ -691,7 +691,7 @@
         classList: ["css-175oi2r", "r-xoduu5"],
         innerHTML: `
           <div class="css-175oi2r r-xoduu5 r-1p0dtai r-1d2f490 r-u8s1d r-zchlnj r-ipm5af r-1niwhzg r-sdzlij r-xf4iuw r-o7ynqc r-6416eg r-1ny4l3l"></div>
-          <svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-50lct3 r-1srniue">
+          <svg viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1xvli5t r-1hdv0qi">
             <g>
               <path d="M8.75 21V3h2v18h-2zM18 21V8.5h2V21h-2zM4 21l.004-10h2L6 21H4zm9.248 0v-7h2v7h-2z" transform="scale(0.75) translate(4, 0)" />
               <path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z" />
@@ -750,9 +750,13 @@
             'a[href*="/status/"]'
           );
           if (tweetEngagementStatusLink) {
-            const tweetEngagementHref =
+            let tweetEngagementHref =
               tweetEngagementStatusLink.getAttribute("href");
+
             if (tweetEngagementHref) {
+              // /photo/1 を除外
+              tweetEngagementHref = tweetEngagementHref.replace("/photo/1", "");
+
               tweetId = tweetEngagementHref;
             }
           }
